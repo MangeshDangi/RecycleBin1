580d319eeff09970d32519fb866f779be5d4cb7f
MANGESH DANGI <101311002+MangeshDangi@users.noreply.github.com>
2025-03-01 17:31:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1111,7 +1111,7 @@ function generateSensorCode() {
     }
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`✅ Server running on port ${PORT}`);
 }).on('error', (error) => {
